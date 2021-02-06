94f6a59d8d58857eb64645f2986081ffa1546e15
tsurumiii <tsurumi0302@gmail.com>
2021-02-06 13:37:56 +0900

    update

--- app/src/main/java/com/volare/mojikore/ui/sample/SampleFragment.kt
+++ app/src/main/java/com/volare/mojikore/ui/sample/SampleFragment.kt
@@ -4,18 +4,20 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
+import androidx.navigation.findNavController
 import com.volare.mojikore.R
+import com.volare.mojikore.databinding.SampleFragmentBinding
 
 class SampleFragment: Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val binding: SampleFragmentBinding = DataBindingUtil.inflate(inflater, R.layout.sample_fragment, container, false)
 
-        val view = inflater.inflate(R.layout.sample_fragment, container, false)
-        view.findViewById<Button>(R.id.home_button).setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_homeFragment)
+        binding.homeButton.setOnClickListener { view: View ->
+            view.findNavController().navigate(R.id.action_FirstFragment_to_homeFragment)
         }
-        return view
+
+        return binding.root
     }
 }
